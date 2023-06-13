9ac7af67533d5599eb8bcf364bb654df20269d10
miguelito0000 <mcopa99@gmail.com>
2023-06-13 11:34:02 -0400

    modificacion de mensaje

--- app/routes/intex.routes.js
+++ app/routes/intex.routes.js
@@ -4,7 +4,7 @@ router.get("/", (rep, res) =>
     res.json('Bienvenidos al curso de usip backend con node.js')
 );
 router.get("/hola2", (rep, res) =>
-    res.json('Hola 2')
+    res.json('Hola 3')
 );
 
 export default router;
